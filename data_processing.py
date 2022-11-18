fe898b0d72baba07505f1723758a90780eed69bf
Johan Sandberg <jvesand@gmail.com>
2022-11-18 12:47:05 +0100

    dashboard infrastructure

--- data_processing.py
+++ data_processing.py
@@ -1,72 +1,122 @@
+import os
 import pandas as pd
 import plotly_express as px
 from functools import reduce
 
-def olymics_plot_df(df: pd.DataFrame, x: str, y: str, grouping: str = None, reverse_sort: bool = False, plot: str = None):
-    """
-    Quick processing of OS data for plotting x on y, with optional grouping.
-    Returns plotly express plot when specified (plot="line", plot="bar" and plot="scatter" supported).
-    Otherwise returns the processed pd.DataFrame for manual creation of plots.
-    """
 
-    if not grouping:
-        # if y is continuous, take mean instead of count
-        if df[y].dtype == 'float64':
-            df = df.groupby(x)[y].mean().reset_index()
+def get_data_path():
+    directory_path = os.path.dirname(__file__)
+    path = os.path.join(directory_path, "Data")
+    return path
+
+
+def load_data(folder_path: str) -> pd.DataFrame:
+    files = []
+
+    for file in ["athlete_events.csv", "noc_regions.csv"]:
+        file_path = os.path.join(folder_path, file)
+        files.append(pd.read_csv(file_path))
+
+    # merge to also get country names
+    # keep all rows from athlete_events.csv, even if NOC is not in noc_regions.csv
+    df = pd.merge(files[0], files[1], on = "NOC", how = "left")
+
+    # there are duplicates for some reason, dropping them
+    df.drop_duplicates(inplace = True)
+
+    # df = df["Name"].apply(lambda x: hl.sha3_256(x.encod()).hexdigest())
+
+    return df
+
+
+class DataProcessing:
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.df = df
+
+    def noc(self, noc: str or list):
+        if isinstance(noc, list):
+            self.df = self.df[self.df["NOC"].isin(noc)]
         else:
-            df = df.groupby(x)[y].count().reset_index()
+            self.df = self.df[self.df["NOC"] == noc]
+        return self
 
-        groups = y
+    def sports(self, sports: str or list):
+        if isinstance(sports, list):
+            self.df = self.df[self.df["Sport"].isin(sports)]
+        else:
+            self.df = self.df[self.df["Sport"] == sports]
+        return self
 
-    else:
-        groups = list(df[grouping].unique())
-        df_list = []
 
-        for i in groups:
-            df_cat = df[df[grouping] == i]
+    def olymics_plot_df(self, x: str, y: str, grouping: str = None, reverse_sort: bool = False, plot: str = None):
+        """
+        Quick processing of OS data for plotting x on y, with optional grouping.
+        Returns plotly express plot when specified (plot="line", plot="bar" and plot="scatter" supported).
+        Otherwise returns the processed pd.DataFrame for manual creation of plots.
+        """
 
-            # if y is linear, take mean instead of count
+        df = self.df
+
+        if not grouping:
+            # if y is continuous, take mean instead of count
             if df[y].dtype == 'float64':
-                df_cat = df_cat.groupby(x)[y].mean().reset_index().rename({y: i}, axis=1)
-                df_list.append(df_cat)
+                df = df.groupby(x)[y].mean().reset_index()
             else:
-                df_cat = df_cat.groupby(x)[y].count().reset_index().rename({y: i}, axis=1)
-                df_list.append(df_cat)
-                
-        # merging a list of dfs
-        # source https://statisticsglobe.com/merge-list-pandas-dataframes-python
-        df = reduce(lambda left, right:
-                            pd.merge(left , right,
-                                    on = [x],
-                                    how = "outer"),
-                            df_list)         
+                df = df.groupby(x)[y].count().reset_index()
 
-    if reverse_sort:                  
-        df = df.sort_values(by = y)
-    else:
-        df = df.sort_values(by = x)
+            groups = y
 
-    if plot == "scatter":
-        fig = px.scatter(
-            df,
-            x = x,
-            y = groups,
-            labels = {"variable": grouping, "value": y})
-        return fig
-    elif plot == "bar":
-        fig = px.bar(
-            df,
-            barmode = "group",
-            x = x,
-            y = groups,
-            labels = {"variable": grouping, "value": y})            
-        return fig
-    elif plot == "line":
-        fig = px.line(
-            df,
-            x = x,
-            y = groups,
-            labels = {"variable": grouping, "value": y})
-        return fig
-    else:
-        return df
+        else:
+            groups = list(df[grouping].unique())
+            df_list = []
+
+            for i in groups:
+                df_cat = df[df[grouping] == i]
+
+                # if y is linear, take mean instead of count
+                if df[y].dtype == 'float64':
+                    df_cat = df_cat.groupby(x)[y].mean().reset_index().rename({y: i}, axis=1)
+                    df_list.append(df_cat)
+                else:
+                    df_cat = df_cat.groupby(x)[y].count().reset_index().rename({y: i}, axis=1)
+                    df_list.append(df_cat)
+                    
+            # merging a list of dfs
+            # source https://statisticsglobe.com/merge-list-pandas-dataframes-python
+            df = reduce(lambda left, right:
+                                pd.merge(left , right,
+                                        on = [x],
+                                        how = "outer"),
+                                df_list)         
+
+        if reverse_sort:                  
+            df = df.sort_values(by = y)
+        else:
+            df = df.sort_values(by = x)
+
+        if plot == "scatter":
+            fig = px.scatter(
+                df,
+                x = x,
+                y = groups,
+                labels = {"variable": grouping, "value": y})
+            return fig
+        elif plot == "bar":
+            fig = px.bar(
+                df,
+                barmode = "group",
+                x = x,
+                y = groups,
+                labels = {"variable": grouping, "value": y})            
+            return fig
+        elif plot == "line":
+            fig = px.line(
+                df,
+                x = x,
+                y = groups,
+                labels = {"variable": grouping, "value": y})
+            return fig
+        else:
+            self.df = df
+            return self
+
---
+++ main.py
@@ -0,0 +1,141 @@
+# add selection of preselected diagrams/themes: chooses called function/methods
+# in those selected themes, the available options can change?
+# clickable box to show average of the measurement (all countries)
+
+# add selection of country China or All
+# add selection of either our selection of sports, or top China sports, or all sports
+# add selection of what to sort on
+# add some bubbles? some other diagrams?
+# reset button?
+
+import dash
+from dash.dependencies import Output, Input
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly_express as px
+
+from data_processing import get_data_path, load_data, DataProcessing
+df = DataProcessing(load_data(get_data_path()))
+
+country_options = [
+    {"label": "China", "value": "China"},
+    {"label": "All countries", "value": "All"},
+]
+
+plot_options = [{"label": i, "value": i} for i in ["line", "bar", "scatter"]]
+
+app = dash.Dash(__name__)
+
+app.layout = html.Main([
+    html.H1("120 years of OS history"),
+    dcc.RadioItems(
+        id = "country-radio",
+        options = country_options,
+        value = "All",
+    ),   
+    html.P("Choose chart"),
+    dcc.Dropdown(
+        id = "chart-dropdown",
+        value = "explorative_all",
+    ),    
+    html.P("Choose x"),
+    dcc.Dropdown(
+        id = "x-dropdown",
+        # options = x_options_dropdown,
+        value = "Games",
+    ),
+    html.P("Choose y"),    
+    dcc.Dropdown(
+        id = "y-dropdown",
+        # options = y_options_dropdown,
+        value = "Medal",
+    ),
+    html.P("Choose grouping/color"),    
+    dcc.Dropdown(
+        id = "grouping-dropdown",
+        # options = grouping_options_dropdown,
+        # value = "Sex",
+    ),
+    dcc.RadioItems(id = "plot-radio", options = plot_options, value = "bar"),
+    dcc.Graph(id = "os-graph"),
+    ]
+)
+
+@app.callback(
+    Output("chart-dropdown", "options"),
+    Input("country-radio", "value"),
+)
+def chart_filter(country):
+    if country == "China":
+        chart_options = [
+                {"label": "Explorative", "value": "explorative"},
+                {"label": "China2", "value": "china2"},  
+        ]
+    else:
+        chart_options = [
+                {"label": "Explorative", "value": "explorative"},
+                {"label": "All2", "value": "all2"},  
+        ]        
+    return chart_options
+
+
+@app.callback(
+    Output("x-dropdown", "options"),
+    Output("y-dropdown", "options"),
+    Output("grouping-dropdown", "options"),
+    Input("chart-dropdown", "value")
+)
+def chart_options_filter(chart):
+    if chart == "china2":
+        x_options_dropdown = [
+            {"label": "test", "value": "test"}
+        ]
+    else:  
+        x_options_dropdown = [
+            {"label": "Olympic games", "value": "Games"},
+            {"label": "Sports", "value": "Sport"},
+            {"label": "Years", "value": "Year"},
+            {"label": "Age", "value": "Age"},
+            {"label": "Height", "value": "Height"},    
+            {"label": "Weight", "value": "Weight"}
+        ]
+    y_options_dropdown = [
+        {"label": "Participants", "value": "ID"},
+        {"label": "Number of medals", "value": "Medal"},
+        {"label": "Age", "value": "Age"},
+        {"label": "Height", "value": "Height"},
+        {"label": "Weight", "value": "Weight"}
+    ]
+    grouping_options_dropdown = [
+        {"label": "Sports", "value": "Sport"},
+        {"label": "Sex", "value": "Sex"},
+        {"label": "Season", "value": "Season"},
+    ]    
+    return x_options_dropdown, y_options_dropdown, grouping_options_dropdown
+
+
+@app.callback(
+    Output("os-graph", "figure"),
+    Input("country-radio", "value"),
+    Input("chart-dropdown", "value"),
+    Input("x-dropdown", "value"),
+    Input("y-dropdown", "value"),
+    Input("grouping-dropdown", "value"),
+    Input("plot-radio", "value"),
+)
+def update_graph(country, chart, x, y, grouping, plot):
+
+    # if chart == "joel_charts":
+        # return df.joel_plots(argument)
+    if country == "China":
+        df = DataProcessing(load_data(get_data_path())).noc("CHN")
+    else: 
+        df = DataProcessing(load_data(get_data_path()))
+
+    return df.olymics_plot_df(x, y, grouping=grouping, plot=plot)
+
+
+
+if __name__ == "__main__":
+    app.run_server(debug = True)
